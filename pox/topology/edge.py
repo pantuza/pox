117955bf80e8eeacaf1f3f18e35967b5d4081cd1
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-07-21 13:44:48 -0700

    Created the Edge class

---
+++ pox/topology/edge.py
@@ -0,0 +1,15 @@
+
+class Edge (GraphEntity):
+  
+  @staticmethod
+  def _make_key (link):
+    if link.entity1.id <= link.entity2.id:
+      return (link.entity1.id, link.entity2.id)
+    else:
+      return (link.entity2.id, link.entity1.id)
+
+  def __init__ (self, link):
+    super(Edge, self).__init__(link)
+    self.key = Edge._make_key(link)
+
+
