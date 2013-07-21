429d37e0d718db49beae986698afbbb3508cfc0f
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-07-21 13:45:05 -0700

    Added the Vertex class

---
+++ pox/topology/vertex.py
@@ -0,0 +1,22 @@
+
+class Vertex (GraphEntity):
+  
+  def __init__ (self, entity):
+    super(Vertex, self).__init__(entity)
+    self.adjacency = {}
+    
+  def add_adjacency (self, vertex, link = None):
+    if vertex != self:
+      try:
+        self.adjacency[vertex].append(link)
+      except:
+        self.adjacency[vertex] = [link]
+
+  def remove_adjacency (self, vertex, link = None):
+    try:
+      self.adjacency[vertex].remove(link)
+      if (self.adjacency[vertex].count == 0):
+        del self.adjacency[vertex]
+    except:
+      pass
+
