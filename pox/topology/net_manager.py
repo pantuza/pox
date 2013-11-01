ef95a740ce97a7fedb2060e197739f0a81f0e915
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-11-01 09:03:11 -0700

    MST intermediary methods
    Implemented the linked() method that verifies if an Edge is
    linked to a vertex that is inside the Minimum Spanning Tree
    
    Implemented the min_edge() method that returns the edge
    with the minimum weight that is connected to a vertex
    from Minimum Spanning Tree

--- pox/topology/net_manager.py
+++ pox/topology/net_manager.py
@@ -9,8 +9,41 @@ class NetManager(object):
 
     self.graph = graph
 
+  def linked(self, edge, mst):
+    """
+    Verifies if an Edge is linked to a vertex in the MST 
+    """
+    source, destination, weight = edge
+
+    for src, dst, wgh in mst:
+      if source in (src, dst) or destination in (src, dst):
+        return True
+    return False
+
+  def min_edge(self, mst, edges):
+    """
+    Returns the edge with the minimum weight and that is linked to 
+    a vertex in the MST
+    """
+    min_edge = None
+    WEIGHT = 2
+
+    for edge in edges:
+      if self.linked(edge, mst):
+
+        # if there isn't a min_edge or the current edge weight is less than 
+        # min_edge weight, then update min_edge with the current edge
+        if min_edge is None or edge[WEIGHT] < min_edge[WEIGHT]:
+          min_edge = edge
+
+    return min_edge
+
   def mst(self):
     """ Finds the Minimum Spanning Tree on the graph
     This is implementation is based on Prim algorithm
     """
-    pass
+
+    mst = []
+    edges = []
+
+     
