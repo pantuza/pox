afb28a07b89f156053ad36e4899a2d738ce07707
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-12-07 00:43:29 -0800

    Added a method for get adjacent list of a vertex

--- pox/topology/vertex.py
+++ pox/topology/vertex.py
@@ -7,17 +7,23 @@ class Vertex (GraphEntity):
     super(Vertex, self).__init__(entity)
     self.adjacency = {}
     
-  def add_adjacency (self, vertex, link = None):
+  def add_adjacency (self, vertex, edge=None):
     if vertex != self:
       try:
-        self.adjacency[vertex].append(link)
+        self.adjacency[vertex].append(edge)
       except:
-        self.adjacency[vertex] = [link]
+        self.adjacency[vertex] = [edge]
 
-  def remove_adjacency (self, vertex, link = None):
+  def remove_adjacency (self, vertex, edge=None):
     try:
-      self.adjacency[vertex].remove(link)
+      self.adjacency[vertex].remove(edge)
       if (self.adjacency[vertex].count == 0):
         del self.adjacency[vertex]
     except:
       pass
+
+  def get_adjacents(self):
+    try:
+      return [(edge.key[1], edge.weight) for edge in self.adjacency.items()]
+    except Exception as e:
+      raise e
